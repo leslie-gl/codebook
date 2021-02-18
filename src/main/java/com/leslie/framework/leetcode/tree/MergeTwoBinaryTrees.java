16cc10d6703219403d46193b2654388da77597b1
leslie <leslie.hudy@outlook.com>
2021-02-18 15:27:50 +0800

    617. Merge Two Binary Trees (Easy)

---
+++ src/main/java/com/leslie/framework/leetcode/tree/MergeTwoBinaryTrees.java
@@ -0,0 +1,49 @@
+package com.leslie.framework.leetcode.tree;
+
+/**
+ * 617. 合并二叉树
+ * 给定两个二叉树，想象当你将它们中的一个覆盖到另一个上时，两个二叉树的一些节点便会重叠。
+ * <p>
+ * 你需要将他们合并为一个新的二叉树。
+ * 合并的规则是:
+ * 如果两个节点重叠，那么将他们的值相加作为节点合并后的新值，否则不为 NULL 的节点将直接作为新二叉树的节点。
+ *
+ * @author leslie
+ * @date 2021/2/18
+ */
+public class MergeTwoBinaryTrees {
+
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) {
+            return null;
+        }
+        if (root1 == null) {
+            return root2;
+        }
+        if (root2 == null) {
+            return root1;
+        }
+        TreeNode leftTree = mergeTrees(root1.left, root2.left);
+        TreeNode rightTree = mergeTrees(root1.right, root2.right);
+        return new TreeNode(root1.val + root2.val, leftTree, rightTree);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
