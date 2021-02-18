761d75dacc3e30202a26a6029d529a7c256056a9
leslie <leslie.hudy@outlook.com>
2021-02-18 16:43:00 +0800

    112. Path Sum (Easy)

---
+++ src/main/java/com/leslie/framework/leetcode/tree/BinaryTreePathSum.java
@@ -0,0 +1,43 @@
+package com.leslie.framework.leetcode.tree;
+
+/**
+ * 112. 路径总和
+ * 给你二叉树的根节点 root 和一个表示目标和的整数 targetSum ，
+ * 判断该树中是否存在 根节点到叶子节点 的路径，这条路径上所有节点值相加等于目标和 targetSum 。
+ * <p>
+ * 叶子节点 是指没有子节点的节点。
+ *
+ * @author leslie
+ * @date 2021/2/18
+ */
+public class BinaryTreePathSum {
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return targetSum == root.val;
+        }
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
