06bf3f728df077f495592ec0089222c5af2395be
leslie <leslie.hudy@outlook.com>
2021-02-19 16:46:05 +0800

    572. Subtree of Another Tree (Easy)

---
+++ src/main/java/com/leslie/framework/leetcode/tree/SubtreeOfAnotherTree.java
@@ -0,0 +1,48 @@
+package com.leslie.framework.leetcode.tree;
+
+/**
+ * 572. 另一个树的子树
+ * 给定两个非空二叉树 s 和 t，检验 s 中是否包含和 t 具有相同结构和节点值的子树。
+ * s 的一个子树包括 t 的一个节点和这个节点的所有子孙。s 也可以看做它自身的一棵子树。
+ *
+ * @author leslie
+ * @date 2021/2/19
+ */
+public class SubtreeOfAnotherTree {
+
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (s == null) {
+            return false;
+        }
+        return isSubtreeWithCurrentNode(s, t) || isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+
+    public boolean isSubtreeWithCurrentNode(TreeNode s, TreeNode t) {
+        if (s == null && t == null) {
+            return true;
+        }
+        if (s == null || t == null || s.val != t.val) {
+            return false;
+        }
+        return isSubtreeWithCurrentNode(s.left, t.left) && isSubtreeWithCurrentNode(s.right, t.right);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
